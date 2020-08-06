3bd87efd5dfd0dee409cd3cf2a83e9804e31a8c6
spacewhales <claude17@naver.com>
2020-08-06 23:28:18 +0900

    데이터와 메서드를 갖는 열거 타입 예시 작성

---
+++ src/com/wonjin/effective/enumtype/WeightTable.java
@@ -0,0 +1,11 @@
+package com.wonjin.effective.enumtype;
+
+public class WeightTable {
+    public static void main(String[] args) {
+        double earthWeight = Double.parseDouble(args[0]);
+        double mass = earthWeight / Planet.EARTH.surfaceGravity();
+        for (Planet planet : Planet.values()) {
+            System.out.printf("%s에서의 무게는 %f이다.%n", planet, planet.surfaceWeight(mass));
+        }
+    }
+}
