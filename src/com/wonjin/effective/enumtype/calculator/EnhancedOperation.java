cdec6270eea6c0aeb3ee7a41cd39ece5e49bf519
spacewhales <claude17@naver.com>
2020-08-09 23:35:01 +0900

    계산기 연산 열거 타입에 symbol과 toString()을 추가
    Operation 생성자에 symbol을 받고 toString() 메서드가 이를 리턴하여 계산식 출력을 편리하게 만들 수 있음.

--- src/com/wonjin/effective/enumtype/calculator/EnhancedOperation.java
+++ src/com/wonjin/effective/enumtype/calculator/EnhancedOperation.java
@@ -6,10 +6,30 @@ package com.wonjin.effective.enumtype.calculator;
     또한 apply 메서드가 추상 메서드이므로 재정의하지 않았다면 컴파일 오류로 알려준다.
  */
 public enum EnhancedOperation {
-    PLUS    {public double apply(double x, double y) {return x + y;}},
-    MINUS   {public double apply(double x, double y) {return x - y;}},
-    TIMES   {public double apply(double x, double y) {return x * y;}},
-    DIVIDE  {public double apply(double x, double y) {return x / y;}};
+    PLUS("+") {
+        public double apply(double x, double y) { return x + y; }
+    },
+    MINUS("-") {
+        public double apply(double x, double y) { return x - y; }
+    },
+    TIMES("*") {
+        public double apply(double x, double y) { return x * y; }
+    },
+    DIVIDE("/") {
+        public double apply(double x, double y) { return x / y; }
+    };
+
+    private final String symbol;
 
     public abstract double apply(double x, double y);
+
+    EnhancedOperation(String symbol) {
+        this.symbol = symbol;
+    }
+
+    // symbol을 추가하고 toString()을 아래와 같이 정의하여 계산식 출력을 편하게 만든다.
+    @Override
+    public String toString() {
+        return symbol;
+    }
 }
